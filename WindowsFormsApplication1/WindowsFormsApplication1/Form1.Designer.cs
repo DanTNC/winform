1cff373b3bdbbc634e2693825d5570df2ba728d2
dantnc <fad11204@yahoo.com.tw>
2018-08-24 22:46:13 +0800

    test event

--- WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -28,12 +28,47 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textbutton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(183, 67);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // textbutton
+            // 
+            this.textbutton.Location = new System.Drawing.Point(141, 260);
+            this.textbutton.Name = "textbutton";
+            this.textbutton.Size = new System.Drawing.Size(135, 73);
+            this.textbutton.TabIndex = 1;
+            this.textbutton.Text = "button1";
+            this.textbutton.UseVisualStyleBackColor = true;
+            this.textbutton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(426, 410);
+            this.Controls.Add(this.textbutton);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "learning";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button textbutton;
     }
 }
 
